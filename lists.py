a7cd2be3b140331449db988fd00f841c7cfd09de
Kilmb <svnalimova88@gmail.com>
2026-01-30 12:21:00 +0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -4,8 +4,8 @@ import os
 
 day_configs = {
     '01.08': {
-        'total_counts': {'ПМ': 60, 'ИВТ': 100, 'ИТСС': 50, 'ИБ': 70},
-        'intersections': {
+        'total': {'ПМ': 60, 'ИВТ': 100, 'ИТСС': 50, 'ИБ': 70},
+        'combos': {
             ('ПМ', 'ИВТ'): 22, ('ПМ', 'ИТСС'): 17, ('ПМ', 'ИБ'): 20,
             ('ИВТ', 'ИТСС'): 19, ('ИВТ', 'ИБ'): 22, ('ИТСС', 'ИБ'): 17,
             ('ПМ', 'ИВТ', 'ИТСС'): 5, ('ПМ', 'ИВТ', 'ИБ'): 5,
@@ -14,8 +14,8 @@ day_configs = {
         }
     },
     '02.08': {
-        'total_counts': {'ПМ': 380, 'ИВТ': 370, 'ИТСС': 350, 'ИБ': 260},
-        'intersections': {
+        'total': {'ПМ': 380, 'ИВТ': 370, 'ИТСС': 350, 'ИБ': 260},
+        'combos': {
             ('ПМ', 'ИВТ'): 190, ('ПМ', 'ИТСС'): 190, ('ПМ', 'ИБ'): 150,
             ('ИВТ', 'ИТСС'): 190, ('ИВТ', 'ИБ'): 140, ('ИТСС', 'ИБ'): 120,
             ('ПМ', 'ИВТ', 'ИТСС'): 70, ('ПМ', 'ИВТ', 'ИБ'): 70,
@@ -24,8 +24,8 @@ day_configs = {
         }
     },
     '03.08': {
-        'total_counts': {'ПМ': 1000, 'ИВТ': 1150, 'ИТСС': 1050, 'ИБ': 800},
-        'intersections': {
+        'total': {'ПМ': 1000, 'ИВТ': 1150, 'ИТСС': 1050, 'ИБ': 800},
+        'combos': {
             ('ПМ', 'ИВТ'): 760, ('ПМ', 'ИТСС'): 600, ('ПМ', 'ИБ'): 410,
             ('ИВТ', 'ИТСС'): 750, ('ИВТ', 'ИБ'): 460, ('ИТСС', 'ИБ'): 500,
             ('ПМ', 'ИВТ', 'ИТСС'): 500, ('ПМ', 'ИВТ', 'ИБ'): 260,
@@ -34,8 +34,8 @@ day_configs = {
         }
     },
     '04.08': {
-        'total_counts': {'ПМ': 1240, 'ИВТ': 1390, 'ИТСС': 1240, 'ИБ': 1190},
-        'intersections': {
+        'total': {'ПМ': 1240, 'ИВТ': 1390, 'ИТСС': 1240, 'ИБ': 1190},
+        'combos': {
             ('ПМ', 'ИВТ'): 1090, ('ПМ', 'ИТСС'): 1110, ('ПМ', 'ИБ'): 1070,
             ('ИВТ', 'ИТСС'): 1050, ('ИВТ', 'ИБ'): 1040, ('ИТСС', 'ИБ'): 1090,
             ('ПМ', 'ИВТ', 'ИТСС'): 1020, ('ПМ', 'ИВТ', 'ИБ'): 1020,
@@ -46,19 +46,15 @@ day_configs = {
 }
 
 
-def create_student(student_id, programs, day):
-    priorities = list(programs)
-    random.shuffle(priorities)
-    priorities += [''] * (4 - len(priorities))
-
+def generate_student_details(day, priorities):
     if day == '01.08':
-        consent_prob = 0.1
+        consent = 0.1
         maths = random.randint(40, 100)
         rus = random.randint(40, 100)
         phys_it = random.randint(41, 100)
         ind = random.randint(0, 10)
     elif day == '02.08':
-        consent_prob = 0.4
+        consent = 0.4
         if 'ИБ' in priorities or 'ИТСС' in priorities:
             maths = random.randint(50, 100)
             rus = random.randint(50, 100)
@@ -70,7 +66,7 @@ def create_student(student_id, programs, day):
             phys_it = random.randint(41, 90)
             ind = random.randint(0, 5)
     elif day == '03.08':
-        consent_prob = 0.4
+        consent = 0.4
         if 'ИБ' in priorities or 'ИТСС' in priorities:
             maths = random.randint(40, 90)
             rus = random.randint(40, 90)
@@ -82,62 +78,149 @@ def create_student(student_id, programs, day):
             phys_it = random.randint(51, 100)
             ind = random.randint(5, 10)
     else:
-        consent_prob = 0.4
-        maths = random.randint(40, 100)
-        rus = random.randint(40, 100)
-        phys_it = random.randint(41, 100)
-        ind = random.randint(0, 10)
+        if 'ПМ' in priorities:
+            maths  = random.randint(85, 100)
+            rus = random.randint(85, 100)
+            phys_it = random.randint(85, 100)
+            ind = random.randint(7, 10)
+        elif 'ИБ' in priorities:
+            maths = random.randint(75, 95)
+            rus = random.randint(75, 95)
+            phys_it = random.randint(75, 95)
+            ind = random.randint(6, 9)
+        elif 'ИВТ' in priorities:
+            maths = random.randint(65, 85)
+            rus = random.randint(65, 85)
+            phys_it = random.randint(65, 85)
+            ind = random.randint(5, 8)
+        else:
+            maths = random.randint(60, 80)
+            rus = random.randint(660, 80)
+            phys_it = random.randint(60, 80)
+            ind = random.randint(0, 6)
+        consent = random.random() < 0.8
 
-    return {
-        'id': student_id,
-        'Математика': maths,
-        'Русский': rus,
-        'Физика/Информатика': phys_it,
-        'Индивидуальные достижения': ind,
-        'Сумма': maths + rus + phys_it + ind,
-        'Согласие': random.random() <= consent_prob,
-        'Приоритет1': priorities[0],
-        'Приоритет2': priorities[1],
-        'Приоритет3': priorities[2],
-        'Приоритет4': priorities[3]
-    }
+
+    return maths, rus, phys_it, ind, consent
 
 
-def generate_day_data(day, config):
-    all_requirements = config['intersections'].copy()
-    for prog, count in config['total_counts'].items():
-        all_requirements[(prog,)] = count
+def get_exclusive_counts(config):
+    targets = config['combos'].copy()
+    for prog, count in config['total'].items():
+        targets[(prog,)] = count
 
-    sorted_groups = sorted(all_requirements.keys(), key=len, reverse=True)
+    sorted_groups = sorted(targets.keys(), key=len, reverse=True)
 
-    pure_counts = {}
+    exclusive_counts = {}
 
     for group in sorted_groups:
-        total_needed = all_requirements[group]
-
+        total_needed = targets[group]
         already_covered = 0
-        for existing_group, count in pure_counts.items():
-            if set(group).issubset(set(existing_group)):
+        for super_group, count in exclusive_counts.items():
+            if set(group).issubset(set(super_group)):
                 already_covered += count
 
-        pure_count = max(0, total_needed - already_covered)
-        pure_counts[group] = pure_count
-    students = []
-    student_id = 1
+        pure_needed = max(0, total_needed - already_covered)
+        exclusive_counts[group] = pure_needed
 
-    for group, count in pure_counts.items():
-        for _ in range(count):
-            students.append(create_student(student_id, group, day))
-            student_id += 1
-
-    df = pd.DataFrame(students)
-    filename = f'{day}.xlsx'
-    df.to_excel(filename, index=False)
+    return exclusive_counts
 
 
 def main():
-    for day in day_configs:
-        generate_day_data(day, day_configs[day])
+    current_students = []
+    next_id = 1
+
+    for day in ['01.08', '02.08', '03.08', '04.08']:
+
+        if current_students:
+            drop_rate = random.uniform(0.05, 0.10)
+            keep_count = int(len(current_students) * (1 - drop_rate))
+            current_students = random.sample(current_students, keep_count)
+
+        for s in current_students:
+            if random.random() < 0.5:
+                if random.random() < 0.2: s['Согласие'] = not s['Согласие']
+                prio = [s.get(f'Приоритет{k}') for k in range(1, 5) if s.get(f'Приоритет{k}')]
+                math, rus, phys_it, ind, consent = generate_student_details(day, prio)
+                s.update({'Математика': math, 'Русский': rus, 'Физика/Информатика': phys_it,
+                          'Индивидуальные достижения': ind, 'Сумма': math + rus + phys_it + ind})
+
+
+        num_existing = len(current_students)
+        num_new = int(max(num_existing * 0.15, 60))
+
+        for _ in range(num_new):
+            current_students.append({
+                'id': next_id,
+                'Приоритет1': None,
+                'Приоритет2': None,
+                'Приоритет3': None,
+                'Приоритет4': None,
+                'Математика': 0,
+                'Русский': 0,
+                'Физика/Информатика': 0,
+                'Индивидуальные достижения': 0,
+                'Сумма': 0,
+                'Согласие': False
+            })
+            next_id += 1
+
+        exclusive_targets = get_exclusive_counts(day_configs[day])
+
+        student_idx = 0
+        total_students_needed = sum(exclusive_targets.values())
+
+        while len(current_students) < total_students_needed:
+            current_students.append({
+                'id': next_id,
+                'Приоритет1': None,
+                'Приоритет2': None,
+                'Приоритет3': None,
+                'Приоритет4': None,
+                'Математика': 0,
+                'Русский': 0,
+                'Физика/Информатика': 0,
+                'Индивидуальные достижения': 0,
+                'Сумма': 0,
+                'Согласие': False
+            })
+            next_id += 1
+
+        random.shuffle(current_students)
+
+        for group, count in exclusive_targets.items():
+            for _ in range(count):
+                if student_idx >= len(current_students):
+                    break
+
+                s = current_students[student_idx]
+                prio_list = list(group)
+                random.shuffle(prio_list)
+                prio_list += [None] * (4 - len(prio_list))
+
+                for k in range(4):
+                    s[f'Приоритет{k + 1}'] = prio_list[k]
+
+                if s['Сумма'] == 0:
+                    math, rus, phys_it, ind, consent = generate_student_details(day, group)
+                    s.update({'Математика': math, 'Русский': rus, 'Физика/Информатика': phys_it,
+                              'Индивидуальные достижения': ind, 'Сумма': math + rus + phys_it + ind, 'Согласие': consent})
+                else:
+                    _, _, _, _, consent = generate_student_details(day, group)
+                    s['Согласие'] = consent
+
+                student_idx += 1
+
+        current_students = current_students[:total_students_needed]
+        df = pd.DataFrame(current_students)
+        df.fillna(value='', inplace=True)
+
+        cols = ['id', 'Математика', 'Русский', 'Физика/Информатика', 'Индивидуальные достижения', 'Сумма', 'Согласие',
+                'Приоритет1', 'Приоритет2', 'Приоритет3', 'Приоритет4']
+        df = df[cols].sort_values('id')
+
+        filename = f'{day}.xlsx'
+        df.to_excel(filename, index=False)
 
 
 if __name__ == "__main__":
