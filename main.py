7b7dcbe6a175558513d7f048b9211af7ff198ab9
Kilmb <svnalimova88@gmail.com>
2026-02-03 20:51:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def upload():
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
 
-        if filename.endswith(('.xlsx')):
+        if filename.endswith('.xlsx'):
             df = pd.read_excel(file_path)
         else:
             flash("Неподдерживаемый формат файла")
@@ -518,4 +518,5 @@ if __name__ == '__main__':
             db.session.add(admin)
             db.session.commit()
         app.run(host='127.0.0.1', port=5000, debug=True)
+
 
